dfc9448790eee558b154e73677ee35f0515a848a
erik-ksth <erikhein.ksth@gmail.com>
2025-03-29 22:28:24 -0700

    update: fetch name on home page

--- app/(app)/home.tsx
+++ app/(app)/home.tsx
@@ -3,6 +3,9 @@ import { useRouter } from 'expo-router';
 import { Colors } from '../../constants/Colors';
 import { Typography } from '../../constants/Typography';
 import { Ionicons } from '@expo/vector-icons';
+import { useEffect, useState } from 'react';
+import { auth, db } from '../../constants/firebaseConfig';
+import { doc, getDoc } from 'firebase/firestore';
 
 const drafts = [
      {
@@ -40,6 +43,21 @@ const previousTrips = [
 
 export default function Home() {
      const router = useRouter();
+     const [userName, setUserName] = useState('');
+
+     useEffect(() => {
+          const fetchUserName = async () => {
+               const user = auth.currentUser;
+               if (user) {
+                    const userDoc = await getDoc(doc(db, "users", user.uid));
+                    if (userDoc.exists()) {
+                         const userData = userDoc.data();
+                         setUserName(userData.firstname);
+                    }
+               }
+          };
+          fetchUserName();
+     }, []);
 
      const goToSettings = () => router.push('/(app)/settings');
      const goToTripPlanning = () => router.push('/(app)/preferences');
@@ -68,7 +86,7 @@ export default function Home() {
                     <View>
                          <View style={styles.nameRow}>
                               <Text style={styles.username}>Hello, </Text>
-                              <Text style={styles.username}>Aung</Text>
+                              <Text style={styles.username}>{userName}</Text>
                          </View>
                          <View style={styles.ecoPointsRow}>
                               <Text style={styles.ecoPoints}>Eco-Points: </Text>
